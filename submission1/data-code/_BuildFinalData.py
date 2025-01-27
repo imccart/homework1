73470ca64a9f10ff3875e553a26c66d244dd1d99
Pablo Estrada <paestrad@espol.edu.ec>
2025-01-27 12:02:50 -0500

    Create _BuildFinalData.py

---
+++ submission1/data-code/_BuildFinalData.py
@@ -0,0 +1,13 @@
+
+# Meta --------------------------------------------------------------------
+# Author:        Pablo Estrada
+# Date Created:  1/27/2025
+# Date Edited:   1/27/2025
+# Notes:         Python file to build Medicare Advantage dataset for Econ/HLTH 470 Hwk 1
+
+
+# Call individual scripts -------------------------------------------------
+
+exec(open("submission1/data-code/1_Plan_Data.py").read())
+
+exec(open("submission1/data-code/2_Service_Areas.py").read())
